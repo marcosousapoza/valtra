a4ad76390a8a4a31769afa57f2a25094c1bf2955
Marco Sousa-Poza <marco.sousapoza@gmail.com>
2025-01-30 15:50:21 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="valtra",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Marco Sousa-Poza",
+    author_email="marco.sousapoza@gmail.com",
     description="A PyTorch DataLoader wrapper for dynamic train-test switching.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/valtra",  # Update this
+    url="https://github.com/marcosousapoza/valtra",
     packages=find_packages(),
     install_requires=["torch>=1.10.0"],
     classifiers=[
